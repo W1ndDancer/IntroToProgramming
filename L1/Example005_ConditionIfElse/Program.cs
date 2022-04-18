2acad59d2f3fbdbcdadccd6bba829ed91aa0393c
WindDancer <v.b.winddancer@gmail.com>
2022-04-19 01:23:51 +0200

    Masha ConditionIfElse added

---
+++ L1/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Как тебя зовут? ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+     Console.WriteLine("Ура! Это же Маша!");
+
+}
+else
+{
+    Console.Write("А...Опять ты ");
+    Console.WriteLine(username);
+}
+
