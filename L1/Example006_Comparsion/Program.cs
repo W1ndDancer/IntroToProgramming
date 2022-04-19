0977de0558663b0ea6bd62d6a1683689ad27a963
WindDancer <v.b.winddancer@gmail.com>
2022-04-19 11:38:10 +0200

    Example006 Comparsion added

---
+++ L1/Example006_Comparsion/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 2;
+int b = 4;
+int c = 8;
+int d = 10;
+int e = 7;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
