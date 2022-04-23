2f17468fef3268f509a81a7cdf31d46f0ea7926f
WindDancer <v.b.winddancer@gmail.com>
2022-04-23 12:27:01 +0200

    Show last numeric added

---
+++ S1/Task5.ShowLastNumericOfThreeDigitNum/Program.cs
@@ -0,0 +1,13 @@
+﻿// Programm will show a last numeric of the three-digit number
+
+Console.WriteLine("Input an integer number");
+int num = Convert.ToInt32(Console.ReadLine());
+int lastNum;
+if (num > 99 && num < 999 || num < -99 && num > -1000)
+{
+    lastNum = Math.Abs(num) % 10;
+    Console.Write("Last numeric of your three-digit number is " + lastNum);
+}
+
+else
+    Console.WriteLine("Bro, its not a three-digit number. Bye!");
