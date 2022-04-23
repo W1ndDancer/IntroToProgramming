6bc813c1cd54ef549e6620bef8c6edd32de6133e
WindDancer <v.b.winddancer@gmail.com>
2022-04-23 10:22:43 +0200

    Task2. Check if numA = numB^2 added

--- S1/Task1.a^2/Program.cs
+++ S1/Task1.a^2/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Hey Bro! Input integer number: ");
+﻿//Task1. The program squares the number
+
+Console.Write("Hey Bro! Input integer number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int result = num*num;
 Console.WriteLine("Bro, result is " + result);
---
+++ S1/Task2.Is_a_equal_b^2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Task2. Check if number A is equal number B^2
+
+Console.Write("Input 1st integer number: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input 2nd integer number: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numA == numB * numB)
+{
+    Console.WriteLine("1st number is equal to square of the 2nd number");
+}
+else
+{
+    Console.WriteLine("1st number is NOT equal to square of the 2nd number");
+}
