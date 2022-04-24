b8cac0ebb9c9e34e09e372a6f530ac24d3427c61
WindDancer <v.b.winddancer@gmail.com>
2022-04-24 20:50:08 +0200

    S2 is started. Tranning Task 1 is done

---
+++ Seminars/S2/Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿// TASK 1. Program must give u a ramdom number in next interval [10;99] and show max numeric of this number.
+
+// 1st var. Using void method
+/*
+void showmMaxNumeric()
+{
+    int num = new Random().Next(10, 100);
+    int num1st = num / 10;
+    int num2nd = num % 10;
+
+    Console.Write(num + " max numeric of this number is ");
+
+    if (num1st > num2nd)
+    {
+        Console.Write(num1st);
+    }
+    if (num1st == num2nd)
+        Console.Write(num1st + " but, numerics of this number is equal");
+    if (num1st < num2nd)
+        Console.Write(num2nd);
+}
+
+showmMaxNumeric();
+*/
+
+// 2nd var. Using Return method
+
+int showmMaxNumeric()
+{
+    int num = new Random().Next(10, 100);
+    int num1st = num / 10;
+    int num2nd = num % 10;
+
+    Console.Write(num + " max numeric of this number is ");
+
+    if (num1st >= num2nd)
+        return num1st;
+    else
+        return num2nd;
+}
+
+int returnedNumber = showmMaxNumeric();
+Console.Write(returnedNumber);
