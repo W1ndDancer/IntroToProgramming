17916367684a4e34f45b129e3676cc1f8fb89394
WindDancer <v.b.winddancer@gmail.com>
2022-04-24 13:57:50 +0200

    Array examples added

---
+++ Lectures/L2/Example009_Array/Program.cs
@@ -0,0 +1,32 @@
+﻿// TASK 1. Programm finds max number using array
+
+// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+
+// int[] array = { 12, 32, 43, 554, 25, 16, 337, 28, 99 };
+
+// int maxArray = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+// Console.Write(maxArray);
+
+// TASK 2. We Have one-dimesional array, what contains N elements, we need to find an element of array what is equal FIND.
+
+int[] array = { 17, 92, 53, 34, 15, 26, 27, 39, 34 };
+int n = array.Length;
+int find = 34;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
