a921c877a708641565639a98b7503927785f1778
WindDancer <v.b.winddancer@gmail.com>
2022-05-14 23:43:52 +0200

    L4 is added

---
+++ Lectures/L4/Example015_matrix/Program.cs
@@ -0,0 +1,44 @@
+﻿// string [,] table = new string [2,5];
+
+// // table[0,0] table[0,1] ... table[0,4]
+// // table[1,0]    ...         table[1,4]  
+
+// table[1,2] = "word";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int colums = 0; colums < 5; colums++)
+//     {
+//         Console.WriteLine($"-{table[rows, colums]}-");
+//     }
+// }
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(0,11);
+        }
+    }
+}
+
+int[,] matr = new int[3, 4];
+
+PrintMatrix(matr);
+FillMatrix(matr);
+Console.WriteLine();
+PrintMatrix(matr);
---
+++ Lectures/L4/Example017_Recursion/Program.cs
@@ -0,0 +1,29 @@
+﻿//Example 17.1 factorial
+
+/*
+double Factorial(int n)
+{
+    if(n==1 || n== 0) return 1;
+    else return n*Factorial(n-1);
+}
+
+for (int i = 0; i <= 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");    
+}
+*/
+
+//Example 17.2 Fibonacci numbers
+
+/*
+int Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
+*/
