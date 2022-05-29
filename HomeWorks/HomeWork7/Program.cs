e1c440962caf64abeb17543692bf5ca14f0c0e41
WindDancer <v.b.winddancer@gmail.com>
2022-05-29 14:55:04 +0200

    H7 is done

---
+++ HomeWorks/HomeWork7/Program.cs
@@ -0,0 +1,140 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+/*
+void FillMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = Convert.ToDouble(new Random().Next(-1000,1001)/100.00);
+        }
+    }
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Input number of rows of matrix. m = ");
+int m = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+Console.Write("Input number of colums of matrix. n = ");
+int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+double[,] newMatrix = new double[m,n];
+
+FillMatrix(newMatrix);
+PrintMatrix(newMatrix);
+*/
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1,7 -> такого числа в массиве нет
+
+/*
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+int[,] newMatrix = new int[5, 5];
+FillMatrix(newMatrix);
+PrintMatrix(newMatrix);
+Console.WriteLine();
+Console.Write("In what row ur element lying? --> ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("And in what column ur element lying? --> ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+if (row < newMatrix.GetLength(0) && column < newMatrix.GetLength(1)) Console.Write("Ur element is --> " + newMatrix[row,column]);
+else Console.Write("Ur element doesn't exist");
+*/
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+/*
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 6);
+        }
+    }
+}
+
+void FindAverage(int[,] matrix)
+{
+    double average = 0;
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            average += matrix[i,j];
+        }
+        average = average/matrix.GetLength(0);
+        Console.WriteLine($"Average summ of {j} column is {Math.Round(average,2)}");
+        average = 0;
+    }
+}
+
+int[,] newMatrix = new int[4, 9];
+FillMatrix(newMatrix);
+PrintMatrix(newMatrix);
+Console.WriteLine();
+FindAverage(newMatrix);
+*/
